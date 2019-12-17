970dcbc6cfef18bd5936af32ad1b97addce0cfe1
Jon Long <jonbrantleylong@gmail.com>
2019-12-17 01:33:17 -0500

    added some styling to the album list

--- src/components/AlbumList/AlbumList.js
+++ src/components/AlbumList/AlbumList.js
@@ -1,11 +1,13 @@
 import React from 'react';
+import './AlbumList.css';
 
 const AlbumList = (props) => {
   return (
     <div className="AlbumList">
       {props.albums.map((a, i) => (
         <div className='album-card' onClick={() => props.getTracks(a.id)}>
-          <p>{a.name}</p>
+          <img src={a.images[0].url} width='100px' height='100px' alt='album art'/>
+          <p>{a.name} by {a.artists[0].name}</p>
         </div>
       ))}
     </div>
